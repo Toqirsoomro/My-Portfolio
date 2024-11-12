b5af5913faa251bd42f1d2836660267212ec1b6b
Toqirsoomro <164857960+Toqirsoomro@users.noreply.github.com>
2024-11-12 14:28:04 +0500

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import { Container } from "postcss";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      Container : {
+        center : true, 
+         padding : "15px",   
+       },
+      colors: {
+      accent : "#08E95E"
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
